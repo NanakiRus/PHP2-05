0d0dfc886219dc821f9bb1529c507fe81116f32d
Maksim Mironov <terrran@gmail.com>
2017-01-31 14:03:56 +0300

    Правка

--- App/Exception/ExceptionDB.php
+++ App/Exception/ExceptionDB.php
@@ -8,11 +8,5 @@ use Exception;
 class ExceptionDB
     extends \Exception
 {
-    public function __construct($message = "", $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-        $textError = $this->getTraceAsString() . "\r\n";
-        file_put_contents('exceptionLog.txt', $textError, FILE_APPEND);
-    }
 
 }
